335268623bea8a14caa89101bb45780184f77322
Atlanswer <atlanswer@gmail.com>
2023-08-30 04:40:51 +0800

    add shadcn ui

--- components/widget/auth.tsx
+++ components/widget/auth.tsx
@@ -1,31 +1,20 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 import { NavbarItem } from "~/components/navbar/item";
+import { Button } from "../ui/button";
 
-export const AuthWidgetContent = () => {
-  return (
-    <>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <LoginButton />
-      </SignedOut>
-    </>
-  );
-};
-
-export const LoginButton = () => <SignInButton />;
-
-// <a
-//   className="box-border whitespace-nowrap rounded border border-gray-200 px-3 py-1 text-sm text-gray-600 transition hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 contrast-more:text-gray-700 dark:border-gray-600 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 contrast-more:dark:text-gray-100"
-//   href="/"
-// >
-//   Login
-// </a>
+export const LoginButton = () => (
+  <SignInButton>
+    <Button className="h-8">Sign in</Button>
+  </SignInButton>
+);
 
 export const AuthWidget = () => (
-  <NavbarItem>
-    <AuthWidgetContent />
-  </NavbarItem>
+  <>
+    <SignedIn>
+      <UserButton />
+    </SignedIn>
+    <SignedOut>
+      <LoginButton />
+    </SignedOut>
+  </>
 );
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/ielts(.*)", "/tip(.*)"],
 });
 
 export const config = {
