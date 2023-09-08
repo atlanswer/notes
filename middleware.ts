305e9804111d140352075c7fd9b6ee1632435db3
Atlanswer <atlanswer@gmail.com>
2023-09-08 23:57:40 +0800

    add notes for unreal

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
+  // publicRoutes: ["/", "/ielts(.*)", "/unreal(.*)", "/tip(.*)"],
   publicRoutes: ["/", "/ielts(.*)", "/tip(.*)"],
 });
 
